666004f9dbc736305703e236149440ba732b58b3
Abstractn <>
2023-06-25 23:26:52 +0200

    Added AbsComponent Typescript reworked version

--- lib/abs-component.ts
+++ lib/abs-component.ts
@@ -1,48 +1,71 @@
-export type AbsComponentInitFunction = (nodeReference: HTMLElement) => HTMLElement;
-
 export interface AbsComponent {
-  init: AbsComponentInitFunction;
-  ready?: Function;
-  [key: string]: any;
-};
-
-export type AbsComponentList = [string[]] | [];
-
-export class AbsComponentManager {
-  static nodeAttributeSelector = 'data-abs-component';
-  static _componentsClassMapper: Record<string, AbsComponent> = {};
-  static components: AbsComponentList = [];
-
-  static registerComponent(templateReferenceName: string, scriptClass: AbsComponent) {
-    this._componentsClassMapper[templateReferenceName] = scriptClass;
-  };
-
-  static initComponent() {};
-  
-  static initComponents() {
-    try {
-      const componentNodeList: NodeListOf<HTMLElement> = document.querySelectorAll(`[${this.nodeAttributeSelector}]`);
-      componentNodeList.forEach(componentNode => {
-        const componentReferenceName = componentNode.getAttribute(this.nodeAttributeSelector) as string;
-        if(this.components[componentReferenceName] === undefined) {
-          this.components[componentReferenceName] = [];
-        }
-        //TODO
-        const componentClass = new this._componentsClassMapper[componentReferenceName];
-        this.components[componentReferenceName].push(componentClass.init(componentNode));
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  init: () => void;
+  ready?: () => void;
+  node: HTMLElement;
 }
 
+export type AbsComponentList = Record<string, AbsComponent[]>;
 
+export class AbsComponentManager {
+  public static nodeAttributeSelector: string = 'data-abs-component';
+  public static components: AbsComponentList = {};
+  private static componentsClassMapper: Record<string, new (node: HTMLElement) => AbsComponent> = {};
 
+  public static registerComponent(templateReferenceName: string, scriptClass: new (node: HTMLElement) => AbsComponent): void {
+    this.componentsClassMapper[templateReferenceName] = scriptClass;
+  }
 
+  public static initComponents(): void {
+    try {
+      document.querySelectorAll(`[${this.nodeAttributeSelector}]`).forEach((componentNode) => {
+        const componentClassName = componentNode.getAttribute(this.nodeAttributeSelector);
+        if(componentClassName === null) throw [`[ABS] The following node's component data attribute value is null:`, componentNode];
+        if(this.componentsClassMapper[componentClassName] === undefined) throw `[ABS] Component initializer error: component "${componentClassName}" is not registered`;
+        if(this.components[componentClassName] === undefined) {
+          this.components[componentClassName] = [];
+        }
+        const ComponentClass = this.componentsClassMapper[componentClassName];
+        const componentInstance = new ComponentClass(componentNode as HTMLElement);
+        componentInstance.init();
+        this.components[componentClassName].push(componentInstance);
+      });
+      Object.keys(this.components).forEach(componentName => {
+        this.components[componentName].forEach(component => {
+          if(component.ready) {
+            component.ready();
+          }
+        })
+      });
+    } catch (error) {
+      if(Array.isArray(error)) {
+        console.error(...error);
+      } else {
+        console.error(error);
+      }
+    }
+  }
 
-class MyCustomComponent implements AbsComponent {
-  init = (test: HTMLElement) => {
-    return test;
+  public static initComponent(componentNode: HTMLElement): void {
+    try {
+      const componentClassName = componentNode.getAttribute(this.nodeAttributeSelector);
+      if(componentClassName === null) throw [`[ABS] The following node's component data attribute value is null:`, componentNode];
+      if(this.componentsClassMapper[componentClassName] === undefined) throw `[ABS] Component initializer error: component "${componentClassName}" is not registered`;
+      if(this.components[componentClassName] === undefined) {
+        this.components[componentClassName] = [];
+      }
+      const ComponentClass = this.componentsClassMapper[componentClassName];
+      const componentInstance = new ComponentClass(componentNode);
+      componentInstance.init();
+      this.components[componentClassName].push(componentInstance);
+      if(componentInstance.ready) {
+        componentInstance.ready();
+      }
+    } catch (error) {
+      if(Array.isArray(error)) {
+        console.error(...error);
+      } else {
+        console.error(error);
+      }
+    }
   }
 }
