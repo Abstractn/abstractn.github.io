cc3a82984afda0bae79759942bc2e5aa721d242c
Abstractn <>
2023-06-29 02:39:32 +0200

    Cleaned AbsTemplate.build with split methods and created ForEach statement

--- lib/abs-template.ts
+++ lib/abs-template.ts
@@ -5,54 +5,149 @@ export enum AbsTemplatePrintMethod {
   AFTER_END = 'afterend',
 };
 
+export type AbsTemplateData = Record<string, string> | Object | any[];
+
 export interface AbsTemplateBuildConfig {
-  templateNode: HTMLElement;
-  templateData?: Record<string, string>;
-  dataMatchExpression?: RegExp;
+  templateNode: HTMLTemplateElement | HTMLElement;
+  templateData?: AbsTemplateData;
   printTargetNode: HTMLElement;
   printMethod: AbsTemplatePrintMethod;
 };
 
-export default class AbsTemplate {
-  public static readonly _DEFAULT_PARAMETER_MATCH_PATTERN: RegExp = /\{\{(.+?)\}\}/;
+export class AbsTemplate {
+  private static readonly CONSOLE_PREFIX: string = '[ABS][TEMPLATE]';
+  public static readonly PARAMETER_MATCH_PATTERN: RegExp = /\{\{(.+?)\}\}/;
+  public static readonly CONDITION_MATCH_PATTERN: RegExp = /\{\{if (!?.+?)|((!?.+?) ==|===|!=|!==|>|>=|<|<=|&&|\|\| (!?.+?))\}\}/;
+  public static readonly CYCLE_MATCH_PATTERN: RegExp = /\{\{forEach (.+?) in (.+?)\}\}(.+?)\{\{\/forEach\}\}/;
   
   public static build(config: AbsTemplateBuildConfig): void {
     try {
-      if(!Boolean(config.templateNode)) throw  `[ABS] "templateNode" in config is null or undefined`;
-      const templateNodeContent = config.templateNode.innerHTML;
+      if(!Boolean(config.templateNode)) throw  `${this.CONSOLE_PREFIX} "templateNode" in config is null or undefined`;
+      let templateNodeContentString = this.getContentFromTemplateNode(config.templateNode);
 
-      const domParser = new DOMParser();
-      const parsedDocument = domParser.parseFromString(templateNodeContent as string, 'text/html');
-      const parsedDocumentBodyNode = parsedDocument.querySelector('body') as HTMLElement;
-      let parsedDocumentBodyContent = parsedDocumentBodyNode.innerHTML;
-
-      if( !(config.templateData === undefined || config.templateData === null) ) {
-        const pattern = config.dataMatchExpression || this._DEFAULT_PARAMETER_MATCH_PATTERN;
-        const parameterGlobalPattern = pattern.global ? pattern : new RegExp(pattern, 'g');
-        const parameterPattern = pattern.global ? new RegExp(pattern, '') : pattern;
-        const matches = parsedDocumentBodyContent.match(parameterGlobalPattern);
-        matches?.forEach(match => {
-          const dataMatches = parameterPattern.exec(match) as Array<string>;
-          const processedMatch = dataMatches[1];
-          const matchedData = (config.templateData as Record<string, string>)[processedMatch];
-          if(matchedData) {
-            parsedDocumentBodyContent = parsedDocumentBodyContent.replace(match, matchedData);
-          }
-        });
+      const isDataDefined = !(config.templateData === undefined || config.templateData === null);
+      if(isDataDefined) {
+        templateNodeContentString = templateNodeContentString.replaceAll('\n', '');
+        templateNodeContentString = this.parse(templateNodeContentString, config.templateData as AbsTemplateData);
       }
 
-      const tempNode = document.createElement('div');
-      tempNode.innerHTML = parsedDocumentBodyContent;
-      const tempNodeContent = Array.from(tempNode.querySelectorAll('*'));
-
-      if(config.printMethod === AbsTemplatePrintMethod.AFTER_BEGIN || config.printMethod === AbsTemplatePrintMethod.AFTER_END) {
-        tempNodeContent.reverse();
-      }
-      tempNodeContent.forEach(node => {
-        config.printTargetNode.insertAdjacentElement(config.printMethod, node);
-      });
+      debugger;
+      const parsedNode = this.util.stringToNode(templateNodeContentString);
+      const mockNode = document.createElement('mock');
+      mockNode.appendChild(parsedNode);
+      const parsedNodeContent = Array.from(mockNode.querySelectorAll('div>*')) as HTMLElement[];
+      
+      this.print(parsedNodeContent, config.printTargetNode, config.printMethod);
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  private static getContentFromTemplateNode = (templateNode: HTMLTemplateElement | HTMLElement): string => {
+    const templateNodeContent = templateNode.innerHTML;
+    const domParser = new DOMParser();
+    const parsedDocument = domParser.parseFromString(templateNodeContent as string, 'text/html');
+    const parsedDocumentBodyNode = parsedDocument.querySelector('body') as HTMLElement;
+    return parsedDocumentBodyNode.innerHTML;
+  }
+
+  private static print(node: HTMLElement | HTMLElement[], target: HTMLElement, method: AbsTemplatePrintMethod): void {
+    if(Array.isArray(node)) {
+      if(method === AbsTemplatePrintMethod.AFTER_BEGIN || method === AbsTemplatePrintMethod.AFTER_END) {
+        node.reverse();
+      }
+      node.forEach(nodeItem => {
+        target.insertAdjacentElement(method, nodeItem);
+      });
+    } else {
+      target.insertAdjacentElement(method, node);
+    }
+  }
+
+  private static getParseMatches = (template: string, pattern: RegExp): RegExpMatchArray | null => {
+    const parameterGlobalPattern = new RegExp(pattern, 'g');
+    const matches = template.match(parameterGlobalPattern);
+    return matches;
+  }
+
+  private static parseParameters(template: string, data: AbsTemplateData, patternOverride?: RegExp): string {
+    const parameterPattern = new RegExp(patternOverride || this.PARAMETER_MATCH_PATTERN, '');
+    const matches = this.getParseMatches(template, patternOverride || this.PARAMETER_MATCH_PATTERN);
+    matches?.forEach(match => {
+      const dataMatches = parameterPattern.exec(match) as Array<string>;
+      const key = dataMatches[1];
+      const keyValue = (data as Record<string, string>)[key];
+      if(keyValue) {
+        template = template.replace(match, keyValue);
+      }
+    });
+    return template;
+  }
+
+  private static parseConditions(template: string, data: AbsTemplateData): string {
+    //BUG if there are multiple constructs of the same type inside each other
+    //the first level will probably match very first closing pattern found (the inner-most construct)
+    //and the result is overlapped
+    //FIXME run a `this.parse()` before anything else
+    //by passing the `conditionContent` of the current match as restricted `template` parameter
+    //as this will work recursively by finding the inner-most level
+    //and leave only the correct closing pattern as last one
+    return template;
+  }
+
+  private static parseCycles(template: string, data: AbsTemplateData): string {
+    //BUG if there are multiple constructs of the same type inside each other
+    //the first level will probably match very first closing pattern found (the inner-most construct)
+    //and the result is overlapped
+    //FIXME run a `this.parse()` before anything else
+    //by passing the `cycleContent` of the current match as restricted `template` parameter
+    //as this will work recursively by finding the inner-most level
+    //and leave only the correct closing pattern as last one
+    const cyclePattern = new RegExp(this.CYCLE_MATCH_PATTERN, '');
+    const matches = this.getParseMatches(template, this.CYCLE_MATCH_PATTERN);
+    matches?.forEach(match => {
+      const matchGroups = cyclePattern.exec(match) as Array<string>;
+      const itemKey = matchGroups[1];
+      const listKey = matchGroups[2];
+      const cycleContent = matchGroups[3];
+
+      const templateData = data as Record<string, string|Object|any[]>;
+      if(templateData) {
+        const list = templateData[listKey];
+        let res = '';
+        if(!Array.isArray(list)) throw `${this.CONSOLE_PREFIX} Template contains a "forEach" with a parameter that cannot be iterated.`;
+        list.forEach(listItem => {
+          //BUG same case for constructs inside each other
+          //an `${itemKey}.(...listItem)` could be found not wrapped by standard curly brackets pattern
+          const subParamRegex = new RegExp(`\\\{\\\{${itemKey}\\.(.+?)\\\}\\\}`,'g');
+          res += this.parseParameters(cycleContent, listItem, subParamRegex);
+        });
+        template = template.replace(match, res);
+      }
+    });
+    return template;
+  }
+
+  private static parse(template: string, data: AbsTemplateData): string {
+    template = this.parseCycles(template, data);
+    template = this.parseConditions(template, data);
+    template = this.parseParameters(template, data);
+    return template;
+  }
+
+  private static util = {
+    nodeToString: (node: HTMLElement): string => {
+      return node.outerHTML.replaceAll('\n', '');
+    },
+    stringToNode: (node: string): HTMLElement => {
+      const resultNode = document.createElement('div');
+      resultNode.innerHTML = node;
+      //return [...(resultNode.querySelectorAll('*'))] as HTMLElement[];
+      return resultNode;
+
+    },
+    removeCharacterFromString: (string: string, characterIndex: number): string => {
+      return string.substring(0, characterIndex) + string.substring(characterIndex + 1, string.length);
     }
   }
 
